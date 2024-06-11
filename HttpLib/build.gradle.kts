053e6f190a850ffd334ab2a76dd4b980be38a5ca
wq12589 <415906626@qq.com>
2024-06-11 22:32:38 +0800

    修改gradle配置

--- HttpLib/build.gradle.kts
+++ HttpLib/build.gradle.kts
@@ -1,120 +1,27 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
-//    id("maven-publish")
-//    `java-library`
-//    alias(libs.plugins.mavenPublish)
     `maven-publish`
-//    `java-library` // 应用java-library插件，它会自动配置sourceSets
-
 }
 
-// 生成源码JAR的task
-//tasks.register("sources", Jar::class) {
-//    archiveClassifier.set("sources")
-////    from(sourceSets.main.get().allSource)
-//    from(sourceSets.get("main").allSource)
-//}
-//afterEvaluate {
-//    android.libraryVariants.each{
-//        publishing.publications.create(variant.name,MavenPublication){
-//            from components.findByName(variant.name)
-//            groupId = 'com.yangzaiarea'//自定义
-//            artifactId = 'yzutils'//自定义
-//            version = '1.0.0'//自定义
-//        }
-//    }
-//}
-//publishing {
-//    publications {
-//        create<MavenPublication>("mavenJava") {
-////            from(components["release"])
-//            from(components["default"])
-//            groupId = "hz.wqq.http.api"
-//            artifactId = "lib"
-//            version = "0.0.2"
-//        }
-//    }
-//}
 
-publishing {
 
-    publications {
-
-        register<MavenPublication>("release") {
-            groupId = "cn.wq"
-            artifactId = "httpLibrary"
-            version = "1.0"
-
-//            afterEvaluate {
-//                from(components["release"])
-//            }
-            // 添加源码JAR到出版物
-//            artifact(tasks["sources"])
-        }
-
-        create<MavenPublication>("mavenJava") {
-            groupId = "cn.wq"
-            artifactId = "httpLibrary"
-            version = "1.0"
-//            from(components["java"])
-//            artifact(tasks["sources"])
-        }
-    }
-//    repositories {
-//        maven {
-//            setUrl("$buildDir/repo")
-//        }
-//    }
-}
-// 定义 sourcesJar 任务
-//val sourcesJar by tasks.registering(Jar::class) {
-//    archiveClassifier.set("sources")
-//    from(android.sourceSets["main"].java.srcDirs)
-//}
-//
-//// 将 sourcesJar 作为一个工件
-//afterEvaluate {
-//    artifacts {
-//        add("archives", sourcesJar.get())
-//    }
-//
-////    publishing {
-////        publications {
-////            create<MavenPublication>("httpLibraryPublication") {
-////                from(components["release"])
-////                artifact(sourcesJar.get())
-////            }
-////        }
-////    }
-//}
 android {
     namespace = "hz.wq.httplib"
     compileSdk = 34
 
     publishing {
-//        multipleVariants {
-//            includeBuildTypeValues("debug", "release")
-//            withSourcesJar()
-//            allVariants()
-//            withJavadocJar()
-//
-//        }
+        multipleVariants {
+            includeBuildTypeValues("debug", "release")
+            withSourcesJar()
+            allVariants()
+            withJavadocJar()
+
+        }
         singleVariant("release") {
             withSourcesJar()
             withJavadocJar()
         }
-//        multipleVariants("http") {
-//            includeBuildTypeValues("debug", "release")
-////            includeFlavorDimensionAndValues(
-////                dimension = "color",
-////                values = arrayOf("blue", "pink")
-////            )
-////            includeFlavorDimensionAndValues(
-////                dimension = "shape",
-////                values = arrayOf("square")
-////            )
-//        }
 
     }
 
@@ -140,7 +47,40 @@ android {
         jvmTarget = "1.8"
     }
 }
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(android.sourceSets["main"].java.srcDirs)
+}
+publishing {
 
+    publications {
+        create<MavenPublication>("httpLibraryPublication") {
+//            from(components["release"])
+
+            // 仅添加一个 sourcesJar
+            artifact(sourcesJar.get())
+        }
+        register<MavenPublication>("release") {
+            groupId = "cn.wq"
+            artifactId = "httpLibrary"
+            version = "1.0"
+
+//            afterEvaluate {
+//                from(components["release"])
+//            }
+            // 添加源码JAR到出版物
+           artifact(sourcesJar.get())
+        }
+//
+//        create<MavenPublication>("mavenJava") {
+//            groupId = "cn.wq"
+//            artifactId = "httpLibrary"
+//            version = "1.0"
+////            from(components["java"])
+////            artifact(tasks["sources"])
+//        }
+    }
+}
 dependencies {
 
     implementation(libs.androidx.core.ktx)
--- build.gradle.kts
+++ build.gradle.kts
@@ -8,63 +8,7 @@ plugins {
 //    alias(libs.plugins.mavenPublish) apply false
 
 }
-//subprojects {
-//    apply(plugin = "maven-publish")
-//}
-//group = "com.example"
-//version = "1.0.0"
 
-//publishing {
-//    publications {
-//        create<MavenPublication>("mavenJava") {
-//            from(components["java"])
-//            artifactId = "my-library"
-//        }
-//    }
-//
-//    repositories {
-//        maven {
-//            url = uri("https://path/to/your/maven/repository")
-//            credentials {
-//                username = project.findProperty("mavenUsername") as String? ?: ""
-//                password = project.findProperty("mavenPassword") as String? ?: ""
-//            }
-//        }
-//    }
-//}
-//publishing {
-////    publications {
-////        create<MavenPublication>("myLibrary") {
-////            from(components["java"])
-////        }
-////    }
-//    publications {
-////        create<MavenPublication>("mavenJava") {
-//        create<MavenPublication>("mavenJava") {
-//            groupId = "hz.wq.testhttpsend"
-//            artifactId = "libraryaaa"
-//            version = "1.0.1"
-//
-//            from(components["java"])
-//        }
-//    }
-////    repositories {
-////        maven {
-////            url = uri("https://path/to/your/maven/repository")
-////            credentials {
-////                username = project.findProperty("mavenUsername") as String? ?: ""
-////                password = project.findProperty("mavenPassword") as String? ?: ""
-////            }
-////        }
-////    }
-//    repositories {
-//        maven {
-//            name = "myRepo"
-//            url = uri(layout.buildDirectory.dir("repo"))
-//        }
-//
-//    }
-//}
-//group = "com.my-company"
-//version = "1.0.2"
+group = "com.my-company"
+version = "1.0.2"
 

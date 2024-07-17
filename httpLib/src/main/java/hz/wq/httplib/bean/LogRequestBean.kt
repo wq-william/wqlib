602734761a44d57b321c01007e2f36bfcf7e9c1d
wangqiang <415906626@qq.com>
2024-07-17 10:46:00 +0800

    修改http log细节

--- httpLib/src/main/java/hz/wq/httplib/bean/LogRequestBean.kt
+++ httpLib/src/main/java/hz/wq/httplib/bean/LogRequestBean.kt
@@ -12,7 +12,7 @@ data class LogRequestBean(
 ) {
     fun getLogString(separate: String = "\n"): String {
         val urlString = "url=$url"
-        val methodString = "method=$method"
+        val methodString = "${separate}method=$method"
         val headsString = if (heads.isNullOrEmpty()) "" else "${separate}heads=$heads"
         val paramsString = if (params.isNullOrEmpty()) "" else "${separate}params=$params"
         val bodyString = if (body.isNullOrEmpty()) "" else "${separate}body=$body"
